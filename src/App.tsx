21d081bb222e52f405038a98169431d27a2bd115
tangbzai <Tan-jc@qq.com>
2023-01-28 00:02:36 +0800

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import styles from './App.module.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+    console.log(1)
+  }, [])
+  return (
+    <div className={styles.App}>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className={styles.logo} alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className={`${styles.logo} ${styles.react}`} alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className={styles.card}>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className={styles.readTheDocs}>
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
