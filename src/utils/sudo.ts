04c030fb84fdaf12a8032812def5638a07f5a198
tangbzai <Tan-jc@qq.com>
2023-03-09 23:00:10 +0800

    feat: 生成一个完整的数独

--- src/utils/sudo.ts
+++ src/utils/sudo.ts
@@ -7,124 +7,228 @@ export type SudoIndex = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
 const DIFFICULTY: 'ease' | 'middle' | 'hard' = 'middle'
 
 /**
- * 获取 0 - 8 的随机数字
- * @param exclude 需要排除的数
+ * 获取一个随机顺序的 1-9 数组
  */
-function getRandomNum(exclude?: SudoIndex[]): SudoIndex {
-  const arr = ([0, 1, 2, 3, 4, 5, 6, 7, 8] as SudoIndex[]).filter(
-    (item) => !exclude?.includes(item)
-  ) as SudoIndex[]
-  return arr[Math.floor(Math.random() * arr.length)]
+function getFullNumBox() {
+  const arr: SudoGroupType = new Array(9)
+    .fill(0)
+    .map((_, index) => index + 1) as SudoGroupType
+  const result: SudoGroupType = []
+  while (arr.length) {
+    result.push(arr.splice(Math.floor(Math.random() * arr.length), 1)[0])
+  }
+  return result
 }
 
 /**
- * 获取每宫的题目
+ * 获取该格可能的数
+ * @param rowPerspective 行视角数独
+ * @param x 横坐标
+ * @param y 纵坐标
  */
-function getBoxProblem(): (SudoValue | null)[] {
-  const arr: SudoValue[] = []
-  let numCount = { ease: 8, middle: 7, hard: 6 }[DIFFICULTY]
-  while (numCount > 0) {
-    if (numCount <= 2 && Math.random() * 2 > 1) {
-      numCount--
-      continue
-    }
-    const index = Math.floor(Math.random() * 9) as SudoIndex
-    arr[index] = (getRandomNum(arr.map((item) => item - 1) as SudoIndex[]) +
-      1) as SudoValue
-    numCount--
+export function getUnitPossible(
+  rowPerspective: SudoProblemType,
+  y: SudoIndex,
+  x: SudoIndex
+): SudoValue[] {
+  const arr: SudoGroupType = new Array(9)
+    .fill(0)
+    .map((_, index) => index + 1) as SudoGroupType
+  const exclude = new Set()
+  for (let i = 0; i < 9; i++) {
+    if (i !== x && rowPerspective[y][i]) exclude.add(rowPerspective[y][i])
+    if (i !== y && rowPerspective[i][x]) exclude.add(rowPerspective[i][x])
+    const [offsetY, offsetX] = [Math.floor(y / 3) * 3, Math.floor(x / 3) * 3]
+    const boxTarget =
+      rowPerspective[offsetY + Math.floor(i / 3)][offsetX + Math.floor(i % 3)]
+    if (boxTarget) exclude.add(boxTarget)
   }
-  return arr
+  return arr.filter((num) => !exclude.has(num)) as SudoValue[]
 }
 
-// FIXME: 目前无法保证生成的题目有且只有唯一解
+/**
+ * 填写只有唯一可能的格子
+ * @param rowPerspective
+ */
+function setUniqueUnit(rowPerspective: SudoProblemType) {
+  rowPerspective.flat(1).forEach((num, index) => {
+    if (num) return
+    const [y, x] = [index / 9, index % 9].map(Math.floor) as [
+      SudoIndex,
+      SudoIndex
+    ]
+    const unitPossible = getUnitPossible(rowPerspective, y, x)
+    if (unitPossible.length === 1) rowPerspective[y][x] = unitPossible[0]
+  })
+}
+
+/**
+ * 检查数独是否可解（存在一格任何数字都不能填则判断为无解）
+ * @param rowPerspective
+ */
+function checkValid(rowPerspective: SudoProblemType) {
+  const hasNotPossible = rowPerspective.flat(1).find((num, index) => {
+    if (num) return false
+    const [y, x] = [index / 9, index % 9].map(Math.floor) as [
+      SudoIndex,
+      SudoIndex
+    ]
+    const unitPossible = getUnitPossible(rowPerspective, y, x)
+    if (!unitPossible.length) return true
+  })
+  return hasNotPossible !== null
+}
+
+/**
+ * 存在一个只有唯一可能格子
+ * @param rowPerspective
+ * @returns
+ */
+function hasUniqueUnit(rowPerspective: SudoProblemType) {
+  const hasUniqueUnit = rowPerspective.flat(1).find((num, index) => {
+    if (num) return false
+    const [y, x] = [index / 9, index % 9].map(Math.floor) as [
+      SudoIndex,
+      SudoIndex
+    ]
+    const unitPossible = getUnitPossible(rowPerspective, y, x)
+    if (unitPossible.length === 1) return true
+  })
+  return hasUniqueUnit === null
+}
+
+/**
+ * 获取空格子列表
+ * @param rowPerspective
+ * @returns
+ */
+function getNullUnitList(rowPerspective: SudoProblemType) {
+  const uniqueUnitList = rowPerspective
+    .flat(1)
+    .map((num, index) => {
+      if (num) return undefined
+      const [y, x] = [index / 9, index % 9].map(Math.floor) as [
+        SudoIndex,
+        SudoIndex
+      ]
+      return {
+        x,
+        y,
+        index,
+      }
+    })
+    .filter((t) => t) as { x: SudoIndex; y: SudoIndex; index: number }[]
+  return uniqueUnitList
+}
+
+/**
+ * 有空格子
+ * @param rowPerspective
+ * @returns
+ */
+function hasNullUnit(rowPerspective: SudoProblemType) {
+  const hasNullUnit = !!rowPerspective.flat(1).find((num) => {
+    if (num) return false
+    return true
+  })
+  return hasNullUnit
+}
+
+/**
+ * 在列表里随机挑一个
+ * @param list
+ * @returns
+ */
+function randomChoice<T extends unknown>(list: T[]): T {
+  const num = list[Math.floor(Math.random() * list.length)]
+  return num
+}
+
+/**
+ * 复制一个数独题目
+ * @param sudoProblem 数独题目
+ * @returns
+ */
+function sudoProblemCopy(sudoProblem: SudoProblemType) {
+  return sudoProblem.map((rows) => rows.map((num) => num))
+}
+
 export function getSudoProblem(): SudoProblemType {
   const sudoProblemByBox: SudoProblemType = []
-  let hasNumBoxCount = { ease: 9, middle: 7, hard: 5 }[DIFFICULTY]
-  while (hasNumBoxCount > 0) {
-    if (hasNumBoxCount < 1 && Math.random() * 2 > 1) {
-      hasNumBoxCount--
+  // 将首尾及中间的宫填上
+  sudoProblemByBox[0] = getFullNumBox()
+  sudoProblemByBox[4] = getFullNumBox()
+  sudoProblemByBox[8] = getFullNumBox()
+
+  // 行视角的数独题目
+  let rowPerspective = transRowPerspective(sudoProblemByBox)
+
+  // 填几个数字减少耗时
+  for (let i = 3 as SudoIndex; i < 9; i += 3) {
+    rowPerspective[i][0] = randomChoice(getUnitPossible(rowPerspective, i, 0))
+    rowPerspective[0][i] = randomChoice(getUnitPossible(rowPerspective, 0, i))
+  }
+
+  // 存储回溯点的栈
+  const recordPointStack: {
+    x: SudoIndex
+    y: SudoIndex
+    sudoProblem: SudoProblemType
+    possibleList: SudoValue[]
+  }[] = []
+  while (!hasNullUnit(rowPerspective)) {
+    if (!checkValid(rowPerspective)) {
+      // 回退到上个回溯点
+      const { y, x, possibleList, sudoProblem } = recordPointStack.splice(
+        -1,
+        1
+      )?.[0]
+      rowPerspective = sudoProblem
+      if (possibleList.length === 1) {
+        rowPerspective[y][x] = possibleList[0]
+        continue
+      }
+      const num = randomChoice(possibleList)
+      rowPerspective[y][x] = num
+      recordPointStack.push({
+        y,
+        x,
+        sudoProblem,
+        possibleList: possibleList.filter((n) => n !== num),
+      })
       continue
     }
-    const index = getRandomNum(
-      sudoProblemByBox.map<SudoIndex>((_, index) => index as SudoIndex)
-    )
-    const boxProblem = getBoxProblem()
-    if (
-      boxProblem.find((item, itemIndex) => {
-        if (!item) return false
-        const [boxItemY, boxItemX] = [Math.floor(itemIndex / 3), itemIndex % 3]
-        // 同一列
-        const columnBoxList = getEqualColumnBox(sudoProblemByBox, index)
-          .map((item) =>
-            item?.filter((_, boxItemIndex) => boxItemIndex % 3 === boxItemX)
-          )
-          .flat(1)
-          .filter((t) => t)
-        if (columnBoxList.includes(item)) return true
-        // 同一行
-        const rowBoxList = getEqualRowBox(sudoProblemByBox, index)
-          .map((item) =>
-            item?.filter(
-              (_, boxItemIndex) => Math.floor(boxItemIndex / 3) === boxItemY
-            )
-          )
-          .flat(1)
-        if (rowBoxList.includes(item)) return true
+    // 如果出现没有唯一可能性时需要随机填一个再继续填
+    if (!hasUniqueUnit(rowPerspective)) {
+      console.warn('not has unique unit need randomChoice')
+      const uniqueUnitList = getNullUnitList(rowPerspective)
+      // uniqueUnitList 为空表示到这步出现无解
+      if (!uniqueUnitList.length) break
+      const { x, y } = randomChoice(uniqueUnitList)
+      const possibleList = getUnitPossible(rowPerspective, y, x)
+      const num = randomChoice(possibleList)
+      // 存储回溯点，含当前数独题目、选择的格子，可能的选择（需要把这次选择的数字排除）
+      recordPointStack.push({
+        x,
+        y,
+        possibleList: possibleList.filter((n) => n !== num),
+        sudoProblem: sudoProblemCopy(rowPerspective),
       })
-    )
+      rowPerspective[y][x] = num
       continue
-    sudoProblemByBox[index] = boxProblem
-    hasNumBoxCount--
+    }
+    setUniqueUnit(rowPerspective)
   }
 
   console.log(
     'numberCount:',
-    sudoProblemByBox.flat(1).reduce<number>((acc, i) => (i ? acc + 1 : acc), 0)
+    rowPerspective
+      .flat(1)
+      .reduce<number>((acc, num) => (num ? acc + 1 : acc), 0)
   )
   // 需要保证最少18个否则可能有多个解
-  // console.log(sudoProblemByBox)
-  return transRowPerspective(sudoProblemByBox)
+  return rowPerspective
 }
-
-/**
- * 获取同列的宫列表
- * @param boxSudoProblem 宫视角数独
- * @param index 当前位置
- * @return 同列的宫列表
- */
-export function getEqualColumnBox(
-  boxSudoProblem: SudoProblemType,
-  index: SudoIndex
-): SudoGroupType[] {
-  const arr: SudoIndex[] = (
-    [0, 3, 6].map((item) => (index % 3) + item) as SudoIndex[]
-  ).filter((item) => item !== index)
-  return arr.map((item) => boxSudoProblem[item])
-}
-
-/**
- * 获取同行的宫
- * @param boxSudoProblem 宫视角数独
- * @param index 当前位置
- * @return 同行的宫列表
- */
-export function getEqualRowBox(
-  boxSudoProblem: SudoProblemType,
-  index: SudoIndex
-): SudoGroupType[] {
-  const arr: SudoIndex[] = (
-    [-2, -1, 1, 2].map((item) => index + item) as SudoIndex[]
-  ).filter(
-    (item) =>
-      Math.floor(index / 3) === Math.floor(item / 3) && item >= 0 && item < 9
-  )
-  return arr.map((item) => boxSudoProblem[item])
-}
-
-/**
- * 获取十字型方向的宫
- */
-export function getEqualCrossBox() {}
 
 /**
  * 转换数独数据展示方式（视角）
