4aae5b4e6e434ee10328c15cada325ead1d5916e
tangbzai <Tan-jc@qq.com>
2023-03-09 23:01:19 +0800

    feat: 展示一格的可能性并在选中一个数字时高亮相同的数字

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,85 @@
 import { useEffect, useState } from 'react'
 import styles from './App.module.css'
-import { getSudoProblem } from './utils/sudo'
+import {
+  getSudoProblem,
+  getUnitPossible,
+  SudoIndex,
+  SudoProblemType,
+  SudoValue,
+} from './utils/sudo'
 
 function App() {
-  const [sudoProblem, setSudoProblem] = useState<(number | null)[][]>(
-    new Array(9).fill(new Array(9).fill(null))
+  const [sudoProblem, setSudoProblem] = useState<SudoProblemType>(
+    new Array(9).fill(new Array(9).fill(undefined))
   )
+  const [showSudo, setShowSudo] = useState<SudoProblemType>(
+    new Array(9).fill(new Array(9).fill(undefined))
+  )
+  const [heightLightNum, setHeightLightNum] = useState<number>()
   useEffect(() => {
     // 获取题目
     const t = new Date()
     const sudoProblem = getSudoProblem()
     console.log('生成耗时：', Date.now() - t.getTime(), 'ms')
     setSudoProblem(sudoProblem)
+    setShowSudo(sudoProblem)
     console.log(sudoProblem)
     // sudoProblem.map((item, index) => console.log(index + 1, item))
   }, [])
+
   return (
     <div className={styles.app}>
-      {sudoProblem.map((row, rowIndex) =>
-        row.map((num, columnIndex) => (
-          <div
-            key={`${rowIndex}-${columnIndex}`}
-            className={styles.unit}
-            contentEditable={!num}
-          >
-            {num}
-          </div>
-        ))
+      {showSudo.map((row, rowIndex) =>
+        row.map((num, columnIndex) => {
+          const [y, x] = [rowIndex, columnIndex] as [SudoIndex, SudoIndex]
+          return (
+            <div
+              key={`${y}-${x}`}
+              className={`${styles.unit} ${
+                heightLightNum === num ? styles.heightLight : ''
+              }`}
+              contentEditable={!sudoProblem[y][x]}
+              onBlur={(e) => {
+                const text = Number(e.target.innerText)
+                const textNum = (
+                  Number.isNaN(text) ? null : text < 1 || text > 9 ? null : text
+                ) as SudoValue
+                const newProblem = showSudo.map((item) => [...item])
+                newProblem[y][x] = textNum
+                setShowSudo(newProblem)
+              }}
+              onClick={(e) => {
+                // @ts-expect-error
+                const text = Number(e.target.innerText)
+                const textNum = Number.isNaN(text) ? undefined : text
+                setHeightLightNum(num ?? textNum ?? undefined)
+              }}
+            >
+              {num ?? <TipsBox numList={getUnitPossible(showSudo, y, x)} />}
+            </div>
+          )
+        })
       )}
+    </div>
+  )
+}
+
+interface TipsBoxProps {
+  numList?: SudoValue[]
+}
+function TipsBox(props: TipsBoxProps) {
+  const numList = props.numList || []
+  return (
+    <div className={styles.tips}>
+      <span style={numList.includes(1) ? {} : { display: 'none' }}>1</span>
+      <span style={numList.includes(2) ? {} : { display: 'none' }}>2</span>
+      <span style={numList.includes(3) ? {} : { display: 'none' }}>3</span>
+      <span style={numList.includes(4) ? {} : { display: 'none' }}>4</span>
+      <span style={numList.includes(5) ? {} : { display: 'none' }}>5</span>
+      <span style={numList.includes(6) ? {} : { display: 'none' }}>6</span>
+      <span style={numList.includes(7) ? {} : { display: 'none' }}>7</span>
+      <span style={numList.includes(8) ? {} : { display: 'none' }}>8</span>
+      <span style={numList.includes(9) ? {} : { display: 'none' }}>9</span>
     </div>
   )
 }
