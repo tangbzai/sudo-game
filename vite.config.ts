46d64795dbb21e907fa9215860cbb9ec696b1fa5
tangbzai <Tan-jc@qq.com>
2023-03-29 00:02:36 +0800

    feat: 增加别名

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  resolve: {
+    alias: {
+      '@': 'src',
+    },
+  },
   server: {
     host: true,
   },
