e8465104dc7f1277e1da4dd8108738666ee43c37
tangbzai <Tan-jc@qq.com>
2023-03-29 00:24:20 +0800

    fix: build error

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '.',
   resolve: {
     alias: {
-      '@': 'src',
+      '@': '/src',
     },
   },
   server: {
